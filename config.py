73ad27401cbb863c0dc1238fdaee083e70ebda5a
CaitlinLi123 <104264689+CaitlinLi123@users.noreply.github.com>
2024-04-15 11:43:00 +0100

    add the latest scraper to the app

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 MAPS_APIKEY = "AIzaSyAX_iqnLB8j7JggiCSHd-pm6RDBBeSbRU0"
 #private_key_path = "C:/Users/31003/Desktop/UCD/comp30830/comp30830.pem"
-private_key_path = "C:/Users/rizal/Documents/UCD/Sem2/COMP30830 SOFTWARE ENGINEERING/comp30830.pem"
+#private_key_path = "C:/Users/rizal/Documents/UCD/Sem2/COMP30830 SOFTWARE ENGINEERING/comp30830.pem"
+private_key_path = '/Users/caitlin/Desktop/comp30830.pem'
