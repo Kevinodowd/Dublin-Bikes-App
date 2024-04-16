bdd2517421e844f8962d77392faf201d655ff55c
CaitlinLi123 <104264689+CaitlinLi123@users.noreply.github.com>
2024-04-16 23:10:00 +0100

    test model_training

--- model_training.py
+++ model_training.py
@@ -39,23 +39,23 @@ def get_model_predict():
 
                 for station, model in models.items():
                     if model:
-                        try:
-                            required_columns = model.feature_names_in_
-                            complete_x = pd.DataFrame(columns=required_columns)
-                            for col in required_columns:
-                                if col in X_train_columns:
-                                    complete_x[col] = X_train[col]
-                                else:
-                                    complete_x[col] = False
+                        #try:
+                        required_columns = model.feature_names_in_
+                        complete_x = pd.DataFrame(columns=required_columns)
+                        for col in required_columns:
+                            if col in X_train_columns:
+                                complete_x[col] = X_train[col]
+                            else:
+                                complete_x[col] = False
 
-                            x_array = np.array(complete_x)
-                            results = model.predict(x_array)
+                        x_array = np.array(complete_x)
+                        results = model.predict(x_array)
 
 
-                            predictions[f'station_{station}'] = {forecastTime[i]: results[i] for i in range(len(forecastTime))}
+                        predictions[f'station_{station}'] = {forecastTime[i]: results[i] for i in range(len(forecastTime))}
 
-                        except Exception as e:
-                            predictions[f'station_{station}'] = None
+                        # except Exception as e:
+                        #     predictions[f'station_{station}'] = None
                     else:
                         predictions[f'station_{station}'] = None
 
